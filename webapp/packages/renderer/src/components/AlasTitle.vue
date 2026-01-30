fbde1cba7c20bf305ae3b765edf7208083003b4c
Megumi <1074176954@qq.com>
2026-01-30 17:11:19 +0800

    调整前端样式

--- webapp/packages/renderer/src/components/AlasTitle.vue
+++ webapp/packages/renderer/src/components/AlasTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="text-6xl font-extralight tracking-wide">
-    <span class="text-7xl text-primary">S</span>tar<span class="text-7xl text-primary">R</span>ail<span class="text-7xl text-primary">C</span>opilot
+    <span class="text-7xl text-primary">B</span>rown<span class="text-7xl text-primary">D</span>ust<span class="text-7xl text-primary">2</span><span class="text-7xl text-primary">A</span>uto<span class="text-7xl text-primary">S</span>script
   </section>
 </template>
 
